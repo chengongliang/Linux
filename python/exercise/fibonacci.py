5f2d07e973727147ad547299c3625e8dcc757e3c
chengongliang <chengongliang7@gmail.com>
2016-07-06 15:03:49 +0800

    Update fibonacci.py

--- python/exercise/fibonacci.py
+++ python/exercise/fibonacci.py
@@ -3,7 +3,7 @@
 def fib(n):
     result = [0,1]
     for i in range(n-2):
-        result = result.append(result[-2]+result[-1])
+        result.append(result[-2]+result[-1])
     return result
 if __name__ == "__main__":
     x = int(raw_input("请输入一个大等于2的数字："))
