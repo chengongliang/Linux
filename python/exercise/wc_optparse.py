e8e28bda0b8d5f4335ff0c1aa2d45e3fed50ce4c
chengongliang <chengongliang7@gmail.com>
2016-07-16 21:50:54 +0800

    Update wc_optparse.py

--- python/exercise/wc_optparse.py
+++ python/exercise/wc_optparse.py
@@ -1,45 +1,57 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from optparse import OptionParser
 import sys
+import os
 
-parser = OptionParser()
-parser.add_option("-c","--char",
-                  dest="chars",
-                  action="store_true",
-                  default=False,
-                  help="only count chars"
-                )
-parser.add_option("-l","--line",
-                  dest="lines",
-                  action="store_true",
-                  default=False,
-                  help="only count lines"
-                )
-parser.add_option("-w","--word",
-                  dest="words",
-                  action="store_true",
-                  default=False,
-                  help="only count words"
-                )
-                
-options, args = parser.parse_args()
-if not (options.lines or options.words or options.chars):
-    options.lines, options.words, options.chars = True, True, True
-if args:
-    fn = args[0]
-    with open(fn) as fd:
-        data = fd.read()
-else:
-    fn = ''
-    data = sys.stdin.read()
-chars = len(data)
-lines = data.count('\n')
-words = len(data.split())
+def opt():
+    parser = OptionParser()
+    parser.add_option("-c","--char",
+                      dest="chars",
+                      action="store_true",
+                      default=False,
+                      help="only count chars")
+    parser.add_option("-w","--word",
+                      dest="words",
+                      action="store_true",
+                      default=False,
+                      help="only count words")
+    parser.add_option("-l","--line",
+                      dest="lines",
+                      action="store_true",
+                      default=False,
+                      help="only count lines")
+    options, args = parser.parse_args()
+    return options, args
 
-if options.lines:
-    print lines,
-if options.words:
-    print words,
-if options.chars:
-    print chars,   
+def get_count(data):
+    chars = len(data)
+    words = len(data.split())
+    lines = data.count('\n')
+    return lines, words ,chars
+
+def print_wc(options, lines, words, chars, fn):
+    if options.lines:
+        print lines,
+    if options.words:
+        print words,
+    if options.chars:
+        print chars,
+    print fn
+
+def main():
+    options, args = opt()
+    if not (options.lines or options.words or options.chars):
+        options.lines, options.words, options.chars = True, True, True
+    if args:
+        fn = args[0]
+        with open(fn) as fd:
+            data = fd.read()
+        lines, words, chars = get_count(data)
+        print_wc(options, lines, words, chars, fn)
+    else:
+        data = sys.stdin.read()
+        fn = ''
+        lines, words, chars = get_count(data)
+        print_wc(options, lines, words, chars, fn)
+main()
