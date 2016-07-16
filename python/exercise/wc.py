43e3b4770da1edfe14d4d1cd26fceb608f6bb9bc
chengongliang <chengongliang7@gmail.com>
2016-07-16 17:13:07 +0800

    Update wc.py

--- python/exercise/wc.py
+++ python/exercise/wc.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python
 #coding:utf8
 
+import os
 import sys
 
-fd = open(sys.argv[1])
-data = fd.read()
+if len(sys.argv) == 1:
+    data = sys.stdin.read()
+else:
+    try:
+        fn = sys.argv[1]
+    except IndexError:
+        print "please follow an argument at %s" % __file__
+        sys.exit()
+    if not os.path.exists(fn):
+        print "%s is not exists" % fn
+        sys.exit()
+    fd = open(sys.argv[1])
+    data = fd.read()
+    fd.close()
+
 chars = len(data)
 words = len(data.split())
 lines = data.count('\n')
-
-print lines,chars,words
+print "%(lines)s %(words)s %(chars)s" % locals()
