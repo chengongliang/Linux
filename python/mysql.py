bffceaf23c6191134bdadd767d0537e38d6b732b
chengongliang <chengongliang7@gmail.com>
2016-08-07 16:57:33 +0800

    Update mysql.py

--- python/mysql.py
+++ python/mysql.py
@@ -6,5 +6,54 @@ from ConfigParser import ConfigParser
 class MySQLConfig(ConfigParser):
     def __init__(self, config, **kw):
         ConfigParser.__init__(self, allow_no_value=True)
+        self.mysqld_vars = {}
+        self.config = config
+        if os.path.exists(self.config):
+            self.read(self.config)
+            self.get_mysqld_vars()
+        else:
+            self.get_defaults_mysqld_vars()
+        self.set_mysqld_vars(kw)
         
-    def     
+    def set_mysqld_vars(self, kw):
+        for k, v in kw.items():
+            setattr(self, k, v)
+            self.mysqld_vars[k] = v
+            
+    def get_mysqld_vars(self):
+        rst = {}
+        options = self.options('mysqld')
+        for o in options:
+            rst[o] = self.get('mysqld', o)
+        self.set_mysqld_vars(rst)
+        
+    def get_defaults_mysqld_vars(self):
+        defaults = {
+            'port':'3306',
+            'socket':'/tmp/mysql.sock',
+            'skip-locking':None,
+            'key_buffer_size':'16K',
+            'max_allowed_packet':'1M',
+            'table_open_cache':'4',
+            'sort_buffer_size':'64K',
+            'read_buffer_size':'256K',
+            'read_rnd_buffer_size':'256K',
+            'net_buffer_length':'2K',
+            'thread_stack':'128K',
+            }
+        self.set_mysqld_vars(defaults)
+        
+    def save(self):
+        if not self.has_section('mysqld'):
+            self.add_section('mysqld')
+        for k, v in self.mysqld_vars.items():
+            self.set('mysqld', k, v)
+        with open(self.config, 'w') as fd:
+            self.write(fd)
+            
+if __name__ == '__main__':
+    mc = MySQLConfig('/etc/my.cnf', max_connections=200, port=3306)
+    mc.save()
+    print mc.max_connections
+    print mc.port
+    print mc.socket
