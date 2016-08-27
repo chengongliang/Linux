1978b5105f95919471702b176296fd0e13daf443
chengongliang <chengongliang7@gmail.com>
2016-08-27 11:10:12 +0800

    Update fabfile.py

--- python/Jenkins/fabfile.py
+++ python/Jenkins/fabfile.py
@@ -3,9 +3,13 @@ from fabric.api import env
 
 env.user = 'test'
 env.password = 'test'
-
-env.hosts = ['localhost','192.168.1.6']
+env.hosts = ['localhost']
 
 def host_type():
-    run('uname -s')
-    run('date')
+    run('uname -s:echo $USER')
+
+def check_var():
+    run("""LIVE_VER=`curl -s http://192.168.11.110/deploy/livever`
+           LIVE_WP=/var/www/deploy/wordpress-$LIVE_VER
+           test -d $LIVE_WP && echo "$LIVE_WP is exists"
+        """)
