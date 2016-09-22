d83ac9e4424a7b5a8083e55f20ed620236d1c938
chengongliang <chengongliang7@gmail.com>
2016-09-22 15:32:36 +0800

    update

--- shell/mysql_backup.sh
+++ shell/mysql_backup.sh
@@ -11,7 +11,7 @@ db_list=`/alidata/server/mysql/bin/mysql -u${user} -p${passwd} -e "show database
 [ -d ${back_dir} ] || mkdir -p ${back_dir}
 for dbname in ${db_list};
 do
-    /alidata/server/mysql/bin/mysqldump -u${user} -p${passwd} --single-transaction -B ${dbname} > ${back_dir}/${dbname}.sql 
+    /alidata/server/mysql/bin/mysqldump -u${user} -p${passwd} --single-transaction -B ${dbname} | gzip > ${back_dir}/${dbname}.sql.gz 
 done
 cd /data/db_back
 if [[ $(pwd) == "/data/db_back" ]];then
