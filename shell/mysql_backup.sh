281b02fe99735d36a71c1967b046b89f7f81a972
chengongliang <chengongliang7@gmail.com>
2016-09-22 10:44:16 +0800

    Create mysql_backup.sh

---
+++ shell/mysql_backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+date=`date +%F`
+Time=`date +%H:%M`
+back_dir=/data/db_back/${date}/${Time}
+user=root
+passwd=password
+
+db_list=`/alidata/server/mysql/bin/mysql -u${user} -p${passwd} -e "show databases;" |grep -Evi "database|performance|information|mysql"`
+
+[ -d ${back_dir} ] || mkdir -p ${back_dir}
+for dbname in ${db_list};
+do
+    /alidata/server/mysql/bin/mysqldump -u${user} -p${passwd} -B ${dbname} > ${back_dir}/${dbname}.sql 
+done
+cd /data/db_back
+if [[ $(pwd) == "/data/db_back" ]];then
+    find . -type d -name "2016*" -mtime +30 | xargs rm -rf
+fi
